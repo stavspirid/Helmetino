af3ee0a83b837b7d18b6f7ebf523ef64aea5628f
stavspirid <stavspirid@gmail.com>
2025-05-19 12:46:16 +0300

    Add Crash Detection

--- helmet/helmet.ino
+++ helmet/helmet.ino
@@ -4,15 +4,6 @@
 
 Adafruit_MPU6050 mpu;
 
-void getMeasurements();
-
-void getAccelX();
-void getAccelY();
-void getAccelZ();
-void getGyroX();
-void getGyroY();
-void getGyroZ();
-
 struct SensorMeasurements {
   float   accelX               =  0.;
   float   accelY               =  0.;
@@ -22,9 +13,22 @@ struct SensorMeasurements {
   float   gyroZ                =  0.;
 } sensorMeasurements;
 
+void getMeasurements();
+void printSensorMeasurements(const SensorMeasurements& s);
+int crashDetection();
+int ax[2], ay[2], az[2];
+
+void getAccelX();
+void getAccelY();
+void getAccelZ();
+void getGyroX();
+void getGyroY();
+void getGyroZ();
+
 sensors_event_t a, g, tmp;
 
-void printSensorMeasurements(const SensorMeasurements& s);
+
+
 
 void setup(void) {
   Serial.begin(115200);
@@ -40,7 +44,13 @@ void loop() {
 
   /* Get new sensor events with the readings */
   getMeasurements();
-
+  
+  if(crashDetection()) {
+    while(true){
+      
+    }
+  }
+  
   Serial.println("");
   delay(500);
 }
@@ -64,6 +74,24 @@ void printSensorMeasurements(const SensorMeasurements& s) {
   Serial.print("Gyroscope X:     "); Serial.println(s.gyroX);
   Serial.print("Gyroscope Y:     "); Serial.println(s.gyroY);
   Serial.print("Gyroscope Z:     "); Serial.println(s.gyroZ);
+}
+
+int crashDetection() {
+  mpu.getEvent(&a, &g, &tmp);
+  ax[0]=(a.acceleration.x);
+  ay[0]=(a.acceleration.y);
+  az[0]=(a.acceleration.z);
+  delay(50);
+  mpu.getEvent(&a, &g, &tmp);
+  ax[1]=(a.acceleration.x);
+  ay[1]=(a.acceleration.y);
+  az[1]=(a.acceleration.z);
+
+  if (abs(ax[0]-ax[1])>1 || abs(ay[0]-ay[1])>1 || abs(az[0]-az[1])>1){
+    Serial.println("CRASH DETECTED");
+    return 1;
+  }
+  return 0;
 }
 
 
